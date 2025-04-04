e08648085804cca86081c6f6bf81db5d99ccb04f
praveenrajofficial0 <praveenrajofficial0@gmail.com>
2025-04-04 10:55:52 +0530

    modified

--- react-store-b/src/App1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function App1(){
-    return(
-        <div style={{backgroundColor:'yellow'}}>App1</div>
-    )
-}
--- react-temp-b/src/App.jsx
+++ react-temp-b/src/App.jsx
@@ -6,6 +6,13 @@ import App3 from './components/App3'
 import App4 from './components/App4'
 import App5 from './components/App5'
 import App6 from './components/App6'
+import App7 from './components/App7'
+import App8 from './components/App8'
+import App9 from './components/App9'
+import App10 from './components/App10'
+import App11 from './components/App11'
+import App12 from './components/App12'
+import App13 from './components/App13'
 function App() {
   return (
     <BrowserRouter>
@@ -17,17 +24,32 @@ function App() {
         <Link to="app4">App4</Link><br/>
         <Link to="app5">App5</Link><br/>
         <Link to="app6">App6</Link><br/>
+        <Link to="app7">App7</Link><br/>
+        <Link to="app8">App8</Link><br/>
+        <Link to="app9">App9</Link><br/>
+        <Link to="app10">App10</Link><br/>
+        <Link to="app11">App11</Link><br/>
+        <Link to="app12">App12</Link><br/>
+        <Link to="app13">App13</Link><br/>
         <hr />
       </div>
       <div>
         <Routes>
           <Route index element={<App1 />}></Route>
-          <Route path="app1" element={<App1 name="Shashank" age="21"/>}></Route>
+          <Route path="app1" element={<App1 name="Praveen Raj" age="21"/>}></Route>
           <Route path="app2" element={<App2 />}></Route>
           <Route path="app3" element={<App3 />}></Route>
           <Route path="app4" element={<App4 />}></Route>
           <Route path="app5" element={<App5 />}></Route>
           <Route path="app6" element={<App6 />}></Route>
+          <Route path="app7" element={<App7 />}></Route>
+          <Route path="app8" element={<App8 />}></Route>
+          <Route path="app9" element={<App9 />}></Route>
+          <Route path="app10" element={<App10 />}></Route>
+          <Route path="app11" element={<App11 />}></Route>
+          <Route path="app12" element={<App12 />}></Route>
+          <Route path="app13" element={<App13 />}></Route>
+
         </Routes>
       </div>
     </div>
